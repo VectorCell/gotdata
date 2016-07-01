a919bd6ba2788a9a643a493704ec6c827264f514
Brandon Smith <vectorcell@gmail.com>
2016-07-01 17:35:27 -0400

    renamed repo dir on VPS

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return app.send_static_file('index.html')
+    # return "Hello, I love Digital Ocean! But I hate everything else."
+
+if __name__ == "__main__":
+    app.run()
