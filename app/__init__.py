39543a787c52de4e8e74a1178943d3897f739bea
Austin Hounsel <austin.hounsel@gmail.com>
2016-07-04 12:50:29 -0400

    Add app/flask.py and modify app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,32 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+@app.route('/')
+def index():
     return app.send_static_file('index.html')
-    # return "Hello, I love Digital Ocean! But I hate everything else."
+
+"""
+@app.route('/static/houses')
+def houses():
+    return app.send_static_file('/static/houses.html')
+
+@app.route('/static/characters')
+def houses():
+    return app.send_static_file('/static/characters.html')
+
+
+@app.route('/static/locations')
+def houses():
+    return app.send_static_file('/static/locations.html')
+
+@app.route('/static/events')
+def events():
+    return app.send_static_file('/static/events.html')
+"""
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    return app.send_static_file(path)
 
 if __name__ == "__main__":
     app.run()
