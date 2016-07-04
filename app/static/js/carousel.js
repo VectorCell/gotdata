35e07710b79e2c48d0348af7d2ec8c2e7f3ed264
Scott <scott.huang.21@outlook.com>
2016-07-04 04:06:40 -0500

    Add to the GUI

--- app/static/js/carousel.js
+++ app/static/js/carousel.js
@@ -1,24 +1,3 @@
-// var $item = $('.carousel .item'); 
-// var $wHeight = $(window).height();
-
-// $item.height($wHeight); 
-// $item.addClass('full-screen');
-
-// $('.carousel img').each(function() {
-//   var $src = $(this).attr('src');
-//   // var $color = $(this).attr('data-color');
-//   $(this).parent().css({
-//     'background-image' : 'url(' + $src + ')',
-//     // 'background-color' : $color
-//   });
-//   $(this).remove();
-// });
-
-// $(window).on('resize', function (){
-//   $wHeight = $(window).height();
-//   $item.height($wHeight);
-// });
-
 $('.carousel').carousel({
   interval: 5000,
   pause: "false"
