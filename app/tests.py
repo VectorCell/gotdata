5a93e78b31d34ba7115f119ed577a9acfe82bb46
Austin Hounsel <austin.hounsel@gmail.com>
2016-07-05 17:12:59 -0400

    Add tests.py

---
+++ app/tests.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from unittest import main, TestCase
+
+class TestGOTData(TestCase):
+
+    def test_get_character_one(self):
+        self.assertEqual(0, 0)
+
+
+if __name__ == "__main__":
+    main()
