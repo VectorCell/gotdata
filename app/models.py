b4132f2cd0b46bfc68b00862771291c954514747
Austin Hounsel <austin.hounsel@gmail.com>
2016-07-07 18:39:42 -0400

    Match models with 'An API of Ice and Fire' (w/o relationships)

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from __init__ import app
 from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.dialects import postgresql
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
 db = SQLAlchemy(app)
@@ -10,42 +9,33 @@ class Character(db.Model):
     __tablename__ = "characters"
 
     # Table attributes
-    id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(256))
+    url = db.Column(db.String(256), primary_key=True)
     name = db.Column(db.String(256))
-    gender = db.Column(db.String(256))
     culture = db.Column(db.String(256))
-    birth_date = db.Column(db.String(256))
-    death_date = db.Column(db.String(256))
+    born = db.Column(db.String(256))
+    died = db.Column(db.String(256))
     father = db.Column(db.String(256))
     mother = db.Column(db.String(256))
     spouse = db.Column(db.String(256))
 
     # Foreign keys
-    house_id = db.Column(db.Integer, db.ForeignKey("houses.id"))
-    location_id = db.Column(db.Integer, db.ForeignKey("locations.id"))
 
     # Relationships
-    house = db.relationship("House", backref=db.backref("characters",
-                                                        lazy="dynamic"))
-    location = db.relationship("Location", backref=db.backref("characters",
-                                                              lazy="dynamic"))
+    # allegiances = db.relationship(
+    # books = db.relationship(
+    # povBooks = db.relationship(
 
-    def __init__(self, url="", name="", gender="", culture="", birth_date="",
-                 death_date="", father="", mother="", spouse="",
-                 house=None, location=None):
+    def __init__(self, url="", name="", culture="", born="",
+                 died="", father="", mother="", spouse=""):
 
         self.url = url
         self.name = name
-        self.gender = gender
         self.culture = culture
-        self.birth_date = birth_date
-        self.death_date = death_date
+        self.born = born
+        self.died = died
         self.father = father
         self.mother = mother
         self.spouse = spouse
-        self.house = house
-        self.location = location
 
     def __repr__(self):
         return "<Character %r>" % self.name
@@ -55,92 +45,70 @@ class House(db.Model):
     __tablename__ = "houses"
 
     # Table attributes
-    id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(256))
+    url = db.Column(db.String(256), primary_key=True)
     name = db.Column(db.String(256))
-    coat_of_arms = db.Column(db.String(256))
+    region = db.Column(db.String(256))
+    coatOfArms = db.Column(db.String(256))
     words = db.Column(db.String(256))
     founded = db.Column(db.String(256))
-    founder = db.Column(db.String(256))
-    died_out = db.Column(db.String(256))
+    diedOut = db.Column(db.String(256))
 
     # Foreign keys
-    location_id = db.Column(db.Integer, db.ForeignKey("locations.id"))
 
     # Relationships
-    location = db.relationship("Location", backref=db.backref("houses",
-                                                              lazy="dynamic"))
+    # currentLord = db.relationship(
+    # heir = db.relationship(
+    # overlord = db.relationship
+    # founder = db.relationship(
+    # cadetBranches = db.relationship(
+    # swornMembers = db.relationship(
 
-    def __init__(self, url="", name="", coat_of_arms="", words="", founded="",
-                 founder="", died_out="", location=None):
+    def __init__(self, url="", name="", region="", coatOfArms="",
+                 words="", founded="", diedOut=""):
 
         self.url = url
         self.name = name
-        self.coat_of_arms = coat_of_arms
+        self.region = region
+        self.coatOfArms = coatOfArms
         self.words = words
         self.founded = founded
-        self.founder = founder
-        self.died_out = died_out
-        self.location = location
+        self.diedOut = diedOut
 
     def __repr__(self):
         return "<House %r>" % self.name
 
 
-# Many-to-many table for events and houses involved
-events_houses = db.Table("events_houses",
-                         db.Column(
-                             "event_id", db.Integer, db.ForeignKey("events.id")),
-                         db.Column(
-                         "house_id", db.Integer, db.ForeignKey("houses.id"))
-                         )
-
-
-class Event(db.Model):
-    __tablename__ = "events"
+class Book(db.Model):
+    __tablename__ = "books"
 
     # Table attributes
-    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(256), primary_key=True)
     name = db.Column(db.String(256))
-    date = db.Column(db.String(256))
+    isbn = db.Column(db.String(256))
+    numberOfPages = db.Column(db.Integer)
+    publisher = db.Column(db.String(256))
+    country = db.Column(db.String(256))
+    mediaType = db.Column(db.String(256))
+    released = db.Column(db.String(256))
 
     # Foreign keys
-    location_id = db.Column(db.Integer, db.ForeignKey("locations.id"))
 
     # Relationships
-    location = db.relationship("Location", backref=db.backref("events",
-                                                              lazy="dynamic"))
+    # characters = db.relationship(
+    # povCharacters = db.relationship(
 
-    def __init__(self, name="", date="", location=None):
+    def __init__(self, url="", name="", isbn="",
+                 numberOfPages=0, publisher="", country="",
+                 mediaType="", released=""):
+
+        self.url = url
         self.name = name
-        self.date = date
-        self.location = location
+        self.isbn = isbn
+        self.numberOfPages = numberOfPages
+        self.publisher = publisher
+        self.country = country
+        self.mediaType = mediaType
+        self.released = released
 
     def __repr__(self):
-        return "<Event %r>" % self.name
-
-
-class Location(db.Model):
-    __tablename__ = "locations"
-
-    # Table attributes
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256))
-    seat = db.Column(db.String(256))
-    seat_type = db.Column(db.String(256))
-    religion = db.Column(db.String(256))
-    population = db.Column(db.String(256))
-    size = db.Column(db.String(256))
-
-    def __init__(self, name="", seat="", seat_type="", religion="",
-                 population="", size=""):
-
-        self.name = name
-        self.seat = seat
-        self.seat_type = seat_type
-        self.religion = religion
-        self.population = population
-        self.size = size
-
-    def __repr__(self):
-        return "<Location %r>" % self.name
+        return "<Book %r>" % self.name
