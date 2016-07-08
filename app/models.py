5ca1e05288a68b2af9cfe639ee80d18318140724
Austin Hounsel <austin.hounsel@gmail.com>
2016-07-08 17:17:03 -0400

    Add postgresql database URI

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from __init__ import app
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://austin:ahh10523@localhost/gotdata"
 db = SQLAlchemy(app)
 
 
