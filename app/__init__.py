b4a31164b04cf7bf3abcab39508860dea84882ea
Scott <scott.huang.21@outlook.com>
2016-07-09 12:53:23 -0500

    Add functionality for templates and example usage in init

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,55 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
     return app.send_static_file('index.html')
 
 @app.route('/character/<int:arg>')
 def character(arg):
     character = {
-        name: arg,
-        img: "",
-        seasons: 1,
-        house: "Lannister",
-        allegiance: "Nope",
-        alive: True,
-        titles: "None",
-        other_names: "None",
-        actor: "Peter Dinklage",
-        current_location: "King's Landing",
-        locations: "None"
+        "name": "Tyrion Lannister",
+        "img": "",
+        "seasons": 1,
+        "house": "Lannister",
+        "allegiance": "Nope",
+        "alive": True,
+        "titles": "None",
+        "other_names": "None",
+        "actor": "Peter Dinklage",
+        "current_location": "King's Landing",
+        "locations": "None"
     }
     return render_template('character.html', character=character)
+
+@app.route('/house/<int:arg>')
+def house(arg):
+    house = {
+        "name": "House Tyrell",
+        "region": "lol",
+        "img": "",
+        "words": "I'm a pig",
+        "lord": "King Joffrey",
+        "heir": "A baby",
+        "overlord": "None",
+        "weapons": "None",
+        "characters": "None",
+        "locations": "None"
+    }
+    return render_template('house.html', house=house)
+
+@app.route('/location/<int:arg>')
+def location(arg):
+    location = {
+        "name": "The Eyrie",
+        "img": "",
+        "people": "None",
+        "ruling_houses": "House Stark",
+        "neighbors": "None",
+        "conflicts": "???"
+    }
+    return render_template('location.html', location=location)
 
 """
 @app.route('/static/houses')
