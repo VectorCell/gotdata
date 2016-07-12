4cc341a246c869747a81ec9b4f754a4eb15e7719
Scott <scott.huang.21@outlook.com>
2016-07-12 18:12:44 -0500

    Minor fix

--- app/static/js/data_table.js
+++ app/static/js/data_table.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $('.pageable-table').each(function() {
         $(this).DataTable();
     });
+    $('.pageable-table-model').each(function() {
+        $(this).DataTable();
+    });
 });
